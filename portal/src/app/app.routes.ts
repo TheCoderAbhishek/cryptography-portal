beefc99a8b9229957cbe96a7817cecea7742710d
Abhishek Patil <abhishek.hpatil63@gmail.com>
2024-08-24 14:30:05 +0530

    create required components in project such as login, navbar, and sidebar

--- portal/src/app/app.routes.ts
+++ portal/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/account/components/login/login.component';
+// import { DashboardComponent } from './features/dashboard/components/';   
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent }
+];
