7e31ebf42e1083114d548b58a3f3b8af8dcc8fbe
Abhishek Patil <abhishek.hpatil63@gmail.com>
2024-08-24 23:35:44 +0530

    Values are read for login

--- portal/src/app/features/account/components/login/login.component.ts
+++ portal/src/app/features/account/components/login/login.component.ts
@@ -1,21 +1,26 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AccountService } from '../../../../infrastructure/account.service';
 
 @Component({
   selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
-  loginForm: FormGroup;
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+  loginError: string | null = null;
 
   constructor(
     private fb: FormBuilder,
     private accountService: AccountService,
     private router: Router
-  ) {
+  ) {}
+
+  ngOnInit(): void {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required]],
@@ -23,22 +28,23 @@ export class LoginComponent {
   }
 
   onSubmit() {
-    if (this.loginForm.valid) {
-      const credentials = {
-        UserEmail: this.loginForm.get('email')?.value,
-        UserPassword: this.loginForm.get('password')?.value,
-      };
+    console.log(this.loginForm.value);
 
-      this.accountService.login(credentials).subscribe(
-        (response) => {
+    if (this.loginForm.valid) {
+      const credentials = this.loginForm.value;
+
+      this.accountService.login(credentials).subscribe({
+        next: (response) => {
           console.log('Login successful:', response);
           this.router.navigate(['/dashboard']);
         },
-        (error) => {
-          alert("Login Failed!");
+        error: (error) => {
+          this.loginError = 'Invalid email or password.';
           console.error('Login failed:', error);
-        }
-      );
+        },
+      });
+    } else {
+      console.log('Form is invalid');
     }
   }
 }
