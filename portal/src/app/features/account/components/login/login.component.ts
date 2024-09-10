f6ffd8067cd4f7f21bfe061d951b7ac8d8f708d4
Abhishek Patil <abhishek.hpatil63@gmail.com>
2024-09-10 08:09:20 +0530

    Add validation for email input box on login page

--- portal/src/app/features/account/components/login/login.component.ts
+++ portal/src/app/features/account/components/login/login.component.ts
@@ -6,13 +6,13 @@ import {
   Validators,
 } from '@angular/forms';
 import { LoginService } from './services/login-service.service';
-import { NgClass } from '@angular/common';
+import { NgClass, NgIf } from '@angular/common';
 import { LoaderService } from '../../../../shared/services/loader.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule, NgClass],
+  imports: [ReactiveFormsModule, NgClass, NgIf],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
@@ -20,6 +20,7 @@ export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
   loginError: string | null = null;
   isPasswordVisible: boolean = false;
+  emailErrorMessage: string | null = null;
   passwordValidation = {
     length: false,
     uppercase: false,
@@ -37,6 +38,7 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
     this.initializeLoginForm();
     this.onPasswordChanges();
+    this.onEmailChanges();
   }
 
   private initializeLoginForm(): void {
@@ -46,24 +48,8 @@ export class LoginComponent implements OnInit {
     });
   }
 
-  onPasswordChanges(): void {
-    this.loginForm.get('userPassword')?.valueChanges.subscribe((password) => {
-      this.passwordValidation.length =
-        password.length >= 8 && password.length <= 20;
-      this.passwordValidation.uppercase = /[A-Z]/.test(password);
-      this.passwordValidation.lowercase = /[a-z]/.test(password);
-      this.passwordValidation.number = /\d/.test(password);
-      this.passwordValidation.specialChar = /[!@#$%^&*(),.?":{}|<>]/.test(
-        password
-      );
-    });
-  }
-
-  togglePasswordVisibility(): void {
-    this.isPasswordVisible = !this.isPasswordVisible;
-  }
-
   onSubmit(): void {
+    this.checkEmailValidity();
     if (this.loginForm.valid) {
       this.loaderService.show();
       const credentials = this.loginForm.value;
@@ -79,5 +65,38 @@ export class LoginComponent implements OnInit {
     } else {
       console.log('Form is invalid');
     }
+  }
+
+  onPasswordChanges(): void {
+    this.loginForm.get('userPassword')?.valueChanges.subscribe((password) => {
+      this.passwordValidation.length =
+        password.length >= 8 && password.length <= 20;
+      this.passwordValidation.uppercase = /[A-Z]/.test(password);
+      this.passwordValidation.lowercase = /[a-z]/.test(password);
+      this.passwordValidation.number = /\d/.test(password);
+      this.passwordValidation.specialChar = /[!@#$%^&*(),.?":{}|<>]/.test(
+        password
+      );
+    });
+  }
+  onEmailChanges(): void {
+    this.loginForm.get('userEmail')?.valueChanges.subscribe(() => {
+      this.checkEmailValidity();
+    });
+  }
+
+  checkEmailValidity(): void {
+    const emailControl = this.loginForm.get('userEmail');
+    if (emailControl?.hasError('required')) {
+      this.emailErrorMessage = 'empty';
+    } else if (emailControl?.hasError('email')) {
+      this.emailErrorMessage = 'invalid';
+    } else {
+      this.emailErrorMessage = null;
+    }
+  }
+
+  togglePasswordVisibility(): void {
+    this.isPasswordVisible = !this.isPasswordVisible;
   }
 }
