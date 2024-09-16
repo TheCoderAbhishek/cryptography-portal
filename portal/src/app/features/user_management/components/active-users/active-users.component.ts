9f9e9120a6863de3ed45b4d53cce19dfc2502f17
Abhishek Patil <abhishek.hpatil63@gmail.com>
2024-09-16 21:10:21 +0530

    Hard Coded DataTable implemented
    -> Design Data table pending
    -> Dynamically fetched data pending in data table.

--- portal/src/app/features/user_management/components/active-users/active-users.component.ts
+++ portal/src/app/features/user_management/components/active-users/active-users.component.ts
@@ -1,12 +1,79 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, AfterViewInit } from '@angular/core';
+import { DataTable } from 'simple-datatables';
+import { isPlatformBrowser } from '@angular/common';
+import { Inject, PLATFORM_ID } from '@angular/core';
 
 @Component({
   selector: 'app-active-users',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './active-users.component.html',
-  styleUrl: './active-users.component.css'
+  styleUrl: './active-users.component.css',
 })
-export class ActiveUsersComponent {
+export class ActiveUsersComponent implements AfterViewInit {
+  users = [
+    {
+      name: 'Flowbite',
+      releaseDate: '2021/25/09',
+      downloads: 269000,
+      growth: '49%',
+    },
+    {
+      name: 'Tailwind CSS',
+      releaseDate: '2021/25/09',
+      downloads: 269000,
+      growth: '49%',
+    },
+    {
+      name: 'Flowbite',
+      releaseDate: '2021/25/09',
+      downloads: 269000,
+      growth: '49%',
+    },
+    {
+      name: 'Flowbite',
+      releaseDate: '2021/25/09',
+      downloads: 269000,
+      growth: '49%',
+    },
+    {
+      name: 'Flowbite',
+      releaseDate: '2021/25/09',
+      downloads: 269000,
+      growth: '49%',
+    },
+    {
+      name: 'Flowbite',
+      releaseDate: '2021/25/09',
+      downloads: 269000,
+      growth: '49%',
+    },
+    {
+      name: 'Flowbite',
+      releaseDate: '2021/25/09',
+      downloads: 269000,
+      growth: '49%',
+    },
+    // Add other user data or fetch dynamically
+  ];
 
+  constructor(@Inject(PLATFORM_ID) private platformId: any) {}
+
+  ngAfterViewInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      const tableElement = document.getElementById('active-user-table');
+      if (tableElement) {
+        new DataTable(tableElement as HTMLTableElement, {
+          searchable: true,
+          perPageSelect: [5, 10, 15],
+          sortable: true,
+          labels: {
+            placeholder: 'Search...',
+            noRows: 'No entries to found',
+          },
+        });
+      }
+    }
+  }
 }
