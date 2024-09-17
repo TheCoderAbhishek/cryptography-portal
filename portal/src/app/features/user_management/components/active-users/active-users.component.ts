b807e6f0d2ba91d666d8e364d30aa47c718bed22
Abhishek Patil <abhishek.hpatil63@gmail.com>
2024-09-17 08:33:32 +0530

    Design adjust for Active Users List Table

--- portal/src/app/features/user_management/components/active-users/active-users.component.ts
+++ portal/src/app/features/user_management/components/active-users/active-users.component.ts
@@ -1,4 +1,4 @@
-import { NgFor, NgIf } from '@angular/common';
+import { NgClass, NgFor, NgIf } from '@angular/common';
 import { Component, AfterViewInit, OnDestroy } from '@angular/core';
 import { DataTable } from 'simple-datatables';
 import { isPlatformBrowser } from '@angular/common';
@@ -13,24 +13,13 @@ interface User {
   email: string;
   isAdmin: boolean;
   isActive: boolean;
-  isLocked: boolean;
-  isDeleted: boolean;
-  loginAttempts: number;
-  deletedStatus: number;
   createdOn: string;
-  updatedOn: string | null;
-  deletedOn: string | null;
-  autoDeletedOn: string | null;
-  lastLoginDateTime: string | null;
-  lockedUntil: string | null;
-  roleId: number;
-  salt: string;
 }
 
 @Component({
   selector: 'app-active-users',
   standalone: true,
-  imports: [NgFor, NgIf],
+  imports: [NgFor, NgIf, NgClass],
   templateUrl: './active-users.component.html',
   styleUrls: ['./active-users.component.css'],
 })
@@ -42,6 +31,25 @@ export class ActiveUsersComponent implements AfterViewInit, OnDestroy {
     @Inject(PLATFORM_ID) private platformId: any,
     private activeUsersService: ActiveUsersService
   ) {}
+
+  initializeDataTable(): void {
+    const tableElement = document.getElementById('active-user-table');
+    if (tableElement) {
+      if (this.dataTable) {
+        this.dataTable.destroy();
+      }
+
+      this.dataTable = new DataTable(tableElement as HTMLTableElement, {
+        searchable: true,
+        perPageSelect: [5, 10, 15],
+        sortable: true,
+        labels: {
+          placeholder: 'Search...',
+          noRows: 'No entries found',
+        },
+      });
+    }
+  }
 
   ngAfterViewInit() {
     if (isPlatformBrowser(this.platformId)) {
@@ -81,30 +89,22 @@ export class ActiveUsersComponent implements AfterViewInit, OnDestroy {
     });
   }
 
-  initializeDataTable(): void {
-    const tableElement = document.getElementById('active-user-table');
-    if (tableElement) {
-      if (this.dataTable) {
-        this.dataTable.destroy();
-      }
-
-      this.dataTable = new DataTable(tableElement as HTMLTableElement, {
-        searchable: true,
-        perPageSelect: [5, 10, 15],
-        sortable: true,
-        labels: {
-          placeholder: 'Search...',
-          noRows: 'No entries found',
-        },
-      });
-    }
-  }
-
   editUser(id: number) {
     console.log('Edit user with ID:', id);
   }
 
   deleteUser(id: number) {
     console.log('Delete user with ID:', id);
+  }
+
+  getRoleName(role: boolean): string {
+    switch (role) {
+      case true:
+        return 'Super Admin';
+      case false:
+        return 'Admin';
+      default:
+        return 'Unknown';
+    }
   }
 }
