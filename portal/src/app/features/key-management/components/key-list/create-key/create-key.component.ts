dc529d187189511f51052e42337acb6f541c10b7
Abhishek Patil <abhishek.hpatil63@gmail.com>
2024-10-20 19:00:39 +0530

    Create required components

---
+++ portal/src/app/features/key-management/components/key-list/create-key/create-key.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-key',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-key.component.html',
+  styleUrl: './create-key.component.css'
+})
+export class CreateKeyComponent {
+
+}
---
+++ portal/src/app/features/key-management/components/key-list/key-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-key-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './key-list.component.html',
+  styleUrl: './key-list.component.css'
+})
+export class KeyListComponent {
+
+}
